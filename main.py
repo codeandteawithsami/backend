0a23eb68bdde1ea4513205d0ea897a5d33bdfd70
Samiullah Saleem <142134499+codeandteawithsami@users.noreply.github.com>
2025-08-21 20:26:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def startup_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
